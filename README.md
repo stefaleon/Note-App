7e5abe696b77e26c492938ccce196065cdda9512
maimous@gmail.com <stefaleon@gmail.com>
2016-11-27 18:23:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#### The Complete Node.js Developer Course 2.0 by Andrew Mead
+
+## Node Fundamentals - Note App
+
+* lodash
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('starting notes.js');
+
+module.exports.addNote = () => {
+	console.log('addNote');
+	return 'New note';
+};
